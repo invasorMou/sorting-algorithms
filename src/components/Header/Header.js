e1ac83094a98031ef5cd0c431272b8745de3a98b
Mauricio Scioville <invasormou@gmail.com>
2020-12-21 16:59:36 -0500

    change anchor to divs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,18 +5,18 @@ const Header = ({ randomizeBars, bubbleSort, mergeSort, quickSort }) => {
   return (
     <div className="container-menu">
       <div className="menu-option">
-        <a onClick={ randomizeBars }>Create Random Array</a>
+        <div onClick={ randomizeBars }>Create Random Array</div>
       </div>
       
       <div className="sort-options">
         <div className="menu-option">
-          <a onClick={ bubbleSort }>Bubble Sort</a>
+          <div onClick={ bubbleSort }>Bubble Sort</div>
         </div>
         <div className="menu-option">
-          <a onClick={ mergeSort }>Merge Sort</a>
+          <div onClick={ mergeSort }>Merge Sort</div>
         </div>
         <div className="menu-option">
-          <a onClick={ quickSort }>Quick Sort</a>
+          <div onClick={ quickSort }>Quick Sort</div>
         </div>
       </div>
     </div>
